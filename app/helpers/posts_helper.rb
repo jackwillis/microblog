1c27c74268b583d70d9457f233854a5f4721a26f
Jack Willis <jack@attac.us>
2016-11-06 18:24:21 -0600

    Update post styles and refactor post helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,20 @@ module PostsHelper
       link_to match, hashtag_path(match[1..-1])
     }
   end
+
+  TIME_FORMAT = "%a, %b %-d, %Y, %l:%S %p".freeze
+  TIME_SWITCH = 2.hours.freeze
+
+  def post_time_tag(post)
+    time = post.created_at
+    now = Time.zone.now
+
+    link = if now.ago(TIME_SWITCH) < time
+      "Posted " + link_to(distance_of_time_in_words(time, now) + " ago", post)
+    else
+      "Posted on " + link_to(time.strftime(TIME_FORMAT), post)
+    end
+
+    time_tag time, link.html_safe
+  end
 end
