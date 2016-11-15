9be28dc3123d3f9e2fb5dd26d1ed50399c3c55db
Jack Willis <jack@attac.us>
2016-11-15 00:37:48 -0600

    Added post like count in views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -20,9 +20,9 @@ module PostsHelper
     now = Time.zone.now
 
     link = if now.ago(2.hours) < time
-      "Posted " + link_to(distance_of_time_in_words(time, now) + " ago", post)
+      link_to(distance_of_time_in_words(time, now) + " ago", post)
     else
-      "Posted on " + link_to(time.strftime("%a, %b %-d, %Y, %l:%S %p"), post)
+      "on " + link_to(time.strftime("%a, %b %-d, %Y, %l:%S %p"), post)
     end
 
     time_tag time, link.html_safe, title: time.rfc2822
