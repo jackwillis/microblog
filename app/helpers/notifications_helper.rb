c5ddf152e8dd160c95bb57142197ef8c4bf583a9
Jack Willis <jack@attac.us>
2016-11-18 20:00:02 -0600

    Refactor render_notification_partial

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,11 @@
 module NotificationsHelper
+  def render_notification_partial(notification)
+    subject_types = [:follow, :post_like]
+
+    locals = Hash[
+      subject_types.map { |s_type| [s_type, notification.send(s_type)] }
+    ]
+
+    render "notifications/#{notification.subject_type.underscore}", locals
+  end
 end
